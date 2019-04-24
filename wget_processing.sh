efbe85285373f907f6f5a31462dfd38cc1f2cab6
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-04-24 14:46:28 -0700

    Create wget_processing.sh

---
+++ wget_processing.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#this must be done before you submit qfit or phenix to SGE since SGE cannot connect to the internet to download PDB!
+
+cd /wynton/home/fraserlab/swankowicz/output
+
+pdb_filelist='/wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/190408_HIV_Prot2.txt'
+while read line; do
+  echo $line
+  PDB=$line
+
+  #make sure you are in the home directory
+  mkdir $PDB
+  cd $PDB
+
+  #download files
+  wget https://files.rcsb.org/download/$PDB.pdb
+  wget https://files.rcsb.org/download/$PDB-sf.cif
+  wget http://edmaps.rcsb.org/coefficients/$PDB.mtz
+done < $pdb_filelist
