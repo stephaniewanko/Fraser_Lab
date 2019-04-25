c252283fc93490f4a61a607c9aa960b7b2e490df
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-04-24 17:13:22 -0700

    completely re-doing this script

--- qfit/qfit_sge_submit.sh
+++ qfit/qfit_sge_submit.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-##$ -l h_vmem=80G
-##$ -l mem_free=64G
-##$ -t 1-5
-##$ -l h_rt=100:00:00
+#$ -l h_vmem=20G
+#$ -l mem_free=20G
+#$ -t 1-5
+#$ -l h_rt=100:00:00
+#$ -pe smp 10
+#$ -R yes
+#$ -V
 
-#This script will be used to submit and parse through qfit.
+echo $NSLOTS
+
+source /wynton/home/fraserlab/swankowicz/phenix-1.15.2-3472/phenix_env.sh
+export PATH="/wynton/home/fraserlab/swankowicz/anaconda3/bin:$PATH"
+source activate qfit
+which python
+
+echo $SGE_TASK_ID
+
+PDB_file=/wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/HIV_Prot2.txt
+
+PDB=$(cat $PDB_file | head -n $SGE_TASK_ID | tail -n 1)
+
+# Run qfit
+cd /wynton/home/fraserlab/swankowicz/output
+cd $PDB
+echo $PDB
+phenix.composite_omit_map ${PDB}.mtz ${PDB}.pdb omit-type=refine nproc = $NSLOTS
+qfit_protein composite_omit_map.mtz -l 2FOFCWT,PH2FOFCWT ${PDB}.pdb -p $NSLOTS
 
 
-input_file=/wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/HIV_Prot2.txt
-for i in {1..6}; do echo $i
-  echo $i
-  PDB=$(cat $input_file | head -n $i | tail -n 1)
-  echo $PDB
-  mkdir $PDB_qfit
-  cd $PDB_qfit
-  wget https://files.rcsb.org/download/$PDB.pdb
-  wget http://edmaps.rcsb.org/coefficients/$PDB.mtz
-  phenix.composite_omit_map ${PDB}_phases.mtz ${PDB}.pdb omit-type=refine
-  qfit_protein composite_omit_map.mtz -l 2FOFCWT,PH2FOFCWT ${PDB}.pdb
-done
+
+
