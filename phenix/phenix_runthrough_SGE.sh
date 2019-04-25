714754e4b706cb0c8da0c6cc09f16c242bb44c22
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-04-25 16:41:59 -0700

    Update phenix_runthrough_SGE.sh

--- phenix/phenix_runthrough_SGE.sh
+++ phenix/phenix_runthrough_SGE.sh
@@ -4,20 +4,27 @@
 #$ -l mem_free=64G
 #$ -t 1-5
 #$ -l h_rt=100:00:00
-#$ -pe smp 6
+#$ -pe smp 8
 #$ -R yes
 #$ -V
 
-echo $NHOSTS
+#________________________________________________INPUTS________________________________________________#
+input_file = /wynton/home/fraserlab/swankowicz/BRD4_output/test.txt #list of PDB names
+base_dir = /wynton/home/fraserlab/swankowicz/BRD4_output/ #location of folders with PDB files
 
+#________________________________________________Activate Env________________________________________________#
+echo $NSLOTS
+echo $SGE_TASK_ID
 source /wynton/home/fraserlab/swankowicz/phenix-1.15.2-3472/phenix_env.sh
-export PATH="/wynton/home/fraserlab/swankowicz/anaconda3/bin:$PATH"
-source activate qfit
+export PATH = "/wynton/home/fraserlab/swankowicz/anaconda3/bin:$PATH"
 source activate phenix_ens
 which python
 
-input_file=/wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/HIV_Prot2.txt
-PDB=$(cat $PDB_file | head -n $SGE_TASK_ID | tail -n 1)
+#________________________________________________RUN PHENIX________________________________________________#
+
+PDB = $(cat $input_file | head -n $SGE_TASK_ID | tail -n 1)
 echo $PDB
-cd /wynton/home/fraserlab/swankowicz/phenix_output/
-mkdir $PDB
+cd $base_dir
+cd $PDB
+/wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/phenix_prep.sh $PDB $NSLOTS
+
