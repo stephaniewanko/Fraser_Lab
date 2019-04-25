7f81603c1320f175ea6b583cf2a7a31b3c365ba1
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-04-25 16:43:46 -0700

    Update qfit_sge_submit.sh

--- qfit/qfit_sge_submit.sh
+++ qfit/qfit_sge_submit.sh
@@ -2,32 +2,43 @@
 
 #$ -l h_vmem=20G
 #$ -l mem_free=20G
-#$ -t 1-5
+#$ -t 01-12
 #$ -l h_rt=100:00:00
-#$ -pe smp 10
+#$ -pe smp 12
 #$ -R yes
 #$ -V
 
-echo $NSLOTS
+#this script will run qfit based on the input PDB names you have.
 
+#________________________________________________INPUTS________________________________________________#
+working_dir = /wynton/home/fraserlab/swankowicz/BRD4_output  #where the folders are located
+PDB_file = /wynton/home/fraserlab/swankowicz/BRD4_output/test.txt  #list of PDB IDs
+
+
+
+#________________________________________________SET PATHS________________________________________________#
 source /wynton/home/fraserlab/swankowicz/phenix-1.15.2-3472/phenix_env.sh
-export PATH="/wynton/home/fraserlab/swankowicz/anaconda3/bin:$PATH"
+export PATH = "/wynton/home/fraserlab/swankowicz/anaconda3/bin:$PATH"
 source activate qfit
-which python
 
+
+#________________________________________________CHECK VARIABLES________________________________________________#
+echo $NSLOTS
+which python
 echo $SGE_TASK_ID
 
-PDB_file=/wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/HIV_Prot2.txt
 
+
+#________________________________________________CHECK VARIABLES________________________________________________#
 PDB=$(cat $PDB_file | head -n $SGE_TASK_ID | tail -n 1)
 
-# Run qfit
-cd /wynton/home/fraserlab/swankowicz/output
+
+
+#________________________________________________RUN QFIT________________________________________________#
+cd $working_dir
 cd $PDB
 echo $PDB
+
 phenix.composite_omit_map ${PDB}.mtz ${PDB}.pdb omit-type=refine nproc = $NSLOTS
 qfit_protein composite_omit_map.mtz -l 2FOFCWT,PH2FOFCWT ${PDB}.pdb -p $NSLOTS
-
-
-
 
