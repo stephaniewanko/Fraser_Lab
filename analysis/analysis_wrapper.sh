318951fe421553305f2010f0d2eb286d87c3a6a5
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-04-30 16:08:01 -0700

    Create analysis_wrapper.sh

---
+++ analysis/analysis_wrapper.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+#________________________________________________INPUTS________________________________________________#
+working_dir='/wynton/home/fraserlab/swankowicz/190430_Apo_Lig_Pairs'  #where the folders are located
+PDB_file=/wynton/home/fraserlab/swankowicz/190430_Apo_Lig_Pairs/List_isomorphous_pairs1.txt  #list of PDB IDs
+
+
+#________________________________________________Activate Env________________________________________________#
+source /wynton/home/fraserlab/swankowicz/phenix-1.15.2-3472/phenix_env.sh
+export PATH="/wynton/home/fraserlab/swankowicz/anaconda3/bin:$PATH"
+source activate phenix_ens
+which python
+
+#________________________________________________RUN PHENIX________________________________________________#
+for i in {1..2}; do
+  PDB=$(cat $PDB_file | head -n $i | tail -n 1)
+
+  echo $PDB
+  cd $working_dir
+  cd $PDB
+  cat 
