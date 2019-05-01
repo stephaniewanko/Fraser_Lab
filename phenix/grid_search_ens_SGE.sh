ce1b65d5277fe1cfa88d0a39442314bf666686ef
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-04-30 20:51:01 -0700

    updating to create new dir for outputs

--- phenix/grid_search_ens_SGE.sh
+++ phenix/grid_search_ens_SGE.sh
@@ -1,31 +1,41 @@
+
 #!/bin/bash
 
 #$ -l h_vmem=80G
 #$ -l mem_free=64G
 #$ -l h_rt=100:00:00
-#$ -pe smp 10
 #$ -t 1-9
 
 #grid search for ensemble_refinement
-#Stephanie Wankowicz 4/15/2019
+#Stephanie Wankowicz 
+#4/30/2019
+
+source /wynton/home/fraserlab/swankowicz/phenix-1.15.2-3472/phenix_env.sh
+export PATH="/wynton/home/fraserlab/swankowicz/anaconda3/bin:$PATH"
+source activate phenix_ens
+which python
+echo $SGE_TASK_ID
 
 
-# we are going to do a grid search of a bunch of different
-#perform grid search: pTLS: 1.0, 0.8, 0.6; weights: wxray_coupled_tbath_offset=2.5;5.0;10.0; timestep always 1.0
-#put in job_title and output_dir based on grid search
-#we are then going to determine which output is better and remove the files related to the poorer output.
-cd /wynton/home/fraserlab/swankowicz/output/2pc0/
-
-##TO ADD: Some log file with Rfree of all ensemble_refinements
 PDB=$1
+cd /wynton/home/fraserlab/swankowicz/190430_Apo_Lig_Pairs/
 echo $PDB
+cd $PDB
 
 options_file=/wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/phenix_ens_grid_search_options.txt
-_pTLS=$(cat $PDB_file | awk '{ print $1 }' head -n $SGE_TASK_ID | tail -n 1)
-_weights=$(cat $PDB_file | awk '{ print $2 }'|head -n $SGE_TASK_ID | tail -n 1)
+_pTLS=$(cat $options_file | awk '{ print $1 }' |head -n $SGE_TASK_ID | tail -n 1)
+_weights=$(cat $options_file | awk '{ print $2 }'|head -n $SGE_TASK_ID | tail -n 1)
 
+echo '_pTLS'
 echo $_pTLS
+echo 'weights'
 echo $_weights
+echo $PWD
+lig_name=$2
+echo $lig_nam
+output_file_name=${PDB}.$_pTLS.$_weights.
+echo $output_file_name
+mkdir $output_file_name
+cd $output_file_name
 
-phenix.ensemble_refinement ${PDB}.updated_refine_001.pdb ${PDB}-sf.mtz elbow.${PDB}_pdb.001.cif pTLS=$_pTLS wxray_coupled_tbath_offset=$_weights ts=1.0 output_file_prefix=${PDB}.{$pTLS}.{$_we$
-
+phenix.ensemble_refinement ../${PDB}.updated_refine_001.pdb ../${PDB}-sf.mtz ../${PDB}.ligands.cif pTLS=$_pTLS wxray_coupled_tbath_offset=$_weights ts=1.0 output_file_prefix=$$
