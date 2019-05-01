50536a91cfa0e3280388b59a41ea174ac1dc7dee
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-05-01 16:44:52 -0700

    Update analysis.py

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -10,42 +10,40 @@ import datetime
 import argparse
 import sys
 
+print(sys.version_info)
 
 def main(PDB, output, RMSD):
-    main_df=pd.read_csv('/wynton/home/fraserlab/swankowicz/190430_Apo_Lig_Pairs/DF_output.txt')
-    summary_df=pd.DataFrame(columns=['PDB','Date', 'Per_Multi_Conf', 'Num_Atom_Multi', 'Total_Atom_Structure', 'Multi_Res')
+    #try else:
+    main_df=pd.read_csv('/wynton/home/fraserlab/swankowicz/190430_Apo_Lig_Pairs/multi_res_summary.csv')
+    print(main_df)
+    summary_df=pd.DataFrame(columns=['PDB','Date', 'Percent_Multi_Conf', 'Num_Atom_Multi', 'Num_Multi_Res'])
     now = datetime.datetime.now()
-    file=open(output, 'r') #take this in as an arguement
-    for line in file:
-        all_atoms=line
-        subset_atoms=line
-        per_multi=line
-    file=open(RMSD, 'r')
-    multi_conf=0
-    for line in file:
-        if line.endswith(2):
-            multi_conf=+1
+    file=pd.read_csv(output, sep=' ', header=None) #take this in as an arguement
+    print('this is the output file:')
+    print(file)
+    all_atoms = float(file.loc[0,0])
+    subset_atoms = float(file.loc[1,0])
+    per_multi = file.loc[2,0]
+    print(all_atoms)
+    file2=pd.read_csv(RMSD, header=None, sep=' ', names=['Residue', 'Chain', 'Per', 'Num_Conf'])
+    subset=(file2.loc[file2['Num_Conf'] == 2])
+    multi_conf=len(subset.index)
     summary_df.loc[1,'PDB']=PDB
-    summary_df.loc[1,'Date']=Now
-    summary_df.loc[1,'Per_Multi_Conf']=per_multi
-    summary_df.loc[1,'Num_Atom_Multi']=subset_atoms
+    summary_df.loc[1,'Date']=now
+    print(per_multi)
+    summary_df.loc[1,'Percent_Multi_Conf']=per_multi
+    print(all_atoms-subset_atoms)
+    summary_df.loc[1,'Num_Atom_Multi']=(all_atoms-subset_atoms)
+    print(multi_conf)
     summary_df.loc[1,'Multi_Res']=multi_conf
     main_df=main_df.append(summary_df)
-    main_df.to_csv('/wynton/home/fraserlab/swankowicz/190430_Apo_Lig_Pairs/DF_output.txt')
-    
+    main_df.to_csv('/wynton/home/fraserlab/swankowicz/190430_Apo_Lig_Pairs/multi_res_summary.csv', index=False)
+
 
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-PDB','--PDB_name', type=float)
-    p.add_argument("-dis", "--distance", type=float, default='1.0',
-                  metavar="<float>", help="Distance from start site to identify ")
-    p.add_argument("-sta", "--starting_site", type=float, default='1.0',
-             metavar="<float>", help="Distance from start site to identify ")
-    p.add_argument("-ls", "--ligand_start", help="Ligand in which you want to measure your distance from")
-    p.add_argument('-output')
-    p.add_argument('-RMSD_file')
-    args = parser.parse_args()
-    main(args.PDB_name, args.output, args.RMSD_file)
-    
+    p = argparse.ArgumentParser()
+    p.add_argument('-PDB','--PDB_name')
+    args = p.parse_args()
+    main(args.PDB_name, 'summary_output.txt', 'ind_rmsd_output.txt')
