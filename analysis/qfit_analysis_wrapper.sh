6c3ec689988aadd2ec8df468cd2821b55dd3f245
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-05-06 22:03:02 -0700

    Update qfit_analysis_wrapper.sh

--- analysis/qfit_analysis_wrapper.sh
+++ analysis/qfit_analysis_wrapper.sh
@@ -13,33 +13,27 @@ source activate phenix_ens
 which python
 
 #________________________________________________RUN PHENIX________________________________________________#
-
-
-for i in {1..2}; do
+for i in {1..20}; do
   apo_PDB=$(cat $apo_PDB_file | head -n $i | tail -n 1)
   echo $apo_PDB
   cd $working_dir
   cd $apo_PDB
   echo 'qfit scripts'
-  generate_single_conformer $apo_PDB.pdb> summary_output.txt
-  get_metrics $apo_PDB.pdb > ind_rmsd_output.txt
+  generate_single_conformer $apo_PDB.pdb> baseline_summary_output.txt
+  get_metrics $apo_PDB.pdb > baseline_ind_rmsd_output.txt
+  generate_single_conformer multiconfomer2.pdb
+  python /wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/analysis.py -PDB=$apo_PDB
   
-  holo_PDB=$(cat $Holo_PDB_file | head -n $i | tail -n 1)
+  holo_PDB=$(cat $holo_PDB_file | head -n $i | tail -n 1)
   echo $holo_PDB
   cd $working_dir
   cd $holo_PDB
   echo 'qfit scripts'
-  generate_single_conformer $holo_PDB.pdb> summary_output.txt
-  get_metrics $holo_PDB.pdb > ind_rmsd_output.txt
+  generate_single_conformer $holo_PDB.pdb> baseline_summary_output.txt
+  get_metrics $holo_PDB.pdb > baseline_ind_rmsd_output.txt
   lig_name=$(cat "ligand_name.txt")
-
-  cd $working_dir
-  cd $holo_PDB  
-  lig_name=$(cat "ligand_name.txt")
-  generate_single_conformer $holo_PDB.pdb> summary_output.txt
-  get_metrics $holo_PDB.pdb > ind_rmsd_output.txt
+  python /wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/analysis.py -PDB=$holo_PDB #-sta=$lig_name -W ignore 
   cd $working_dir
   subset_structure -apo_structure ./$apo_PDB/$apo_PDB.pdb -holo_structure ./$holo_PDB/$holo_PDB.pdb -lig $lig_name -dis	5
-  python /wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/analysis.py -PDB=$holo_PDB #-sta=$lig_name -W ignore
 done
 
