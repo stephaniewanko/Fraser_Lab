2354964339fb140888d285908898d30cf720a2a4
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-07-25 17:22:53 -0700

    Update parse_refine_log.py
    remove heuristic portion

--- phenix/parse_refine_log.py
+++ phenix/parse_refine_log.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#last edited: 2019-04-12
+#last edited: 2019-07-24
 #last edited by: Stephanie Wankowicz
 
 import pandas as pd
@@ -10,32 +10,32 @@ import sys
 
 
 #parse refinement output .log
-def parse_refine_log(file):
-    file=open(file, 'r') #take this in as an arguement
+def parse_refine_log(file, PDB):
+    file = open(file, 'r') #take this in as an arguement
     global refine_df
-    refine_df=pd.DataFrame(columns=['PDB','Date', 'Starting_Rfree', 'Ending_Rfree', 'Starting_Rwork', 'Ending_Rwork', 'Number of Refinement', 'delta_Rwork', 'delta_Rfree'])
+    refine_df = pd.DataFrame(columns=['PDB','Date', 'Starting_Rfree', 'Ending_Rfree', 'Starting_Rwork', 'Ending_Rwork', 'Number of Refinement', 'delta_Rwork', 'delta_Rfree'])
     now = datetime.datetime.now()
     for line in file:
         if line.startswith('Command line arguments'):
             #print(line)
-            pdb_name=line.split('.')[0][-4:]
+            pdb_name = line.split('.')[0][-4:]
             #print(pdb_name)
         elif line.startswith('Start R-work'):
-            start_rwork=float(line.split('=')[1][1:7])
-            start_rfree=float(line.split('=')[2][1:7])
+            start_rwork = float(line.split('=')[1][1:7])
+            start_rfree = float(line.split('=')[2][1:7])
             #print(start_rfree)
         elif line.startswith('Final R-work'):
-            final_rwork=float(line.split('=')[1][1:7])
-            final_rfree=float(line.split('=')[2][1:7])
-    refine_df.loc[1,'PDB']=pdb_name
-    refine_df.loc[1,'Starting_Rfree']=start_rfree
-    refine_df.loc[1,'Ending_Rfree']=final_rfree
-    refine_df.loc[1,'Starting_Rwork']=start_rwork
-    refine_df.loc[1,'Ending_Rwork']=final_rwork
-    refine_df.loc[1,'Number of Refinement']=2
+            final_rwork = float(line.split('=')[1][1:7])
+            final_rfree = float(line.split('=')[2][1:7])
+    refine_df.loc[1,'PDB'] = PDB
+    refine_df.loc[1,'Starting_Rfree'] = start_rfree
+    refine_df.loc[1,'Ending_Rfree'] = final_rfree
+    refine_df.loc[1,'Starting_Rwork'] = start_rwork
+    refine_df.loc[1,'Ending_Rwork'] = final_rwork
+    refine_df.loc[1,'Number of Refinement'] = 8
     refine_df.loc[1,'delta_Rwork'] = refine_df.loc[1,'Starting_Rwork']-refine_df.loc[1,'Ending_Rwork']
-    refine_df.loc[1, 'R_Rfree_start']=refine_df.loc[1,'Starting_Rwork']-refine_df.loc[1,'Starting_Rfree']
-    refine_df.loc[1, 'R_Rfree_end']=refine_df.loc[1,'Ending_Rwork']-refine_df.loc[1,'Ending_Rfree']
+    refine_df.loc[1, 'R_Rfree_start'] = refine_df.loc[1,'Starting_Rwork']-refine_df.loc[1,'Starting_Rfree']
+    refine_df.loc[1, 'R_Rfree_end'] = refine_df.loc[1,'Ending_Rwork']-refine_df.loc[1,'Ending_Rfree']
     refine_df.loc[1,'delta_Rfree'] = refine_df.loc[1,'Starting_Rfree']-refine_df.loc[1,'Ending_Rfree']
     #refine_df.loc[1, 'Rfree_thres']=((float(refine_df.loc[1,'Starting_Rfree'])*0.02)+float(refine_df.loc[1,'Starting_Rfree']))
     #refine_df.loc[1,'Rdiff_thres']=((float(refine_df.loc[1,'R_Rfree_start'])*0.02)+float(refine_df.loc[1,'R_Rfree_start']))
@@ -74,17 +74,10 @@ def heuristics_pass(refine_df):
             #file.write(float(refine_df.loc[1,'R_Rfree_end']))
 
 
-def main(file):
-    PDB=parse_refine_log(file)
-    heuristics_pass(refine_df)
-    refine_df.to_csv(PDB + '_refine_df.csv')
-    print(float(refine_df.loc[1,'R_Rfree_start']))
-    print(float(refine_df.loc[1,'R_Rfree_end']))
-    #return('Continue')
-    sys.exit(0)
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename')
+    parser.add_argument('-log_file')
+    p.add_argument('-PDB')
     args = parser.parse_args()
-    main(args.filename)
+    parse_refine_log(args.log_file, args.PDB)
+    refine_df.to_csv(args.PDB+'_refine_df.csv')
