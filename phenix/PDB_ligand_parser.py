353a5834fac0ba99312a16a8058c19eda9510987
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-08-12 14:25:42 -0700

    Update PDB_ligand_parser.py

--- phenix/PDB_ligand_parser.py
+++ phenix/PDB_ligand_parser.py
@@ -21,22 +21,37 @@ def get_lig_name(PDB,lig_list):
     ppdb.read_pdb(PDB+'.pdb')
     HETATM=ppdb.df['HETATM']
     residue_names = set(HETATM['residue_name'])
+    lig_res_number = 0
     atoms = 0 #base number of atoms for a ligand
+    all_ligands=[]
+    _lig_name = ''
     for i in residue_names:
+        print(i)
+        print(type(i))
+        all_ligands.append(i)
         if i in set(lig_list['Lig_name']):
             continue
         else:
             subset=(ppdb.df['HETATM']['residue_name']==i)
             lig_res_nubmer = subset.values.sum()
-            if lig_res_number > atoms:
+            if lig_res_number >= atoms:
               #print(subset.values.sum())
               _lig_name = i
               atoms = lig_res_nubmer
             else:
               continue
+    print(all_ligands)
+    print(all_ligands[0])
     with open('ligand_name.txt', 'w') as file:
         file.write(_lig_name)
+    with open('ligand_list.txt', 'w') as file:
+        for i in range(0,len(all_ligands)):
+            print(i)
+            file.write(str(all_ligands[i])+' and ')#+"\n")
+        #file.write(str(all_ligands[1]))+"\n")
+        #file.write(str(all_ligands[2]))+"\n")
     return _lig_name
+
 
 def main(PDB, lig_list):
     ligand_list = pd.read_csv(lig_list)
