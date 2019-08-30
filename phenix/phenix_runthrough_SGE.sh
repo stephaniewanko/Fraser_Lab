719b7fbb99ce6f3dd538f061b3e93cb9315c06bd
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-08-30 11:59:52 -0700

    Update phenix_runthrough_SGE.sh
    updated base_dir option

--- phenix/phenix_runthrough_SGE.sh
+++ phenix/phenix_runthrough_SGE.sh
@@ -4,19 +4,25 @@
 #$ -l mem_free=64G
 #$ -t 1-5
 #$ -l h_rt=100:00:00
-#$ -pe smp 8
+#$ -pe smp 2
 #$ -R yes
 #$ -V
 
 #________________________________________________INPUTS________________________________________________#
-input_file = /wynton/home/fraserlab/swankowicz/BRD4_output/test.txt #list of PDB names
-base_dir = /wynton/home/fraserlab/swankowicz/BRD4_output/ #location of folders with PDB files
+input_file=/wynton/home/fraserlab/swankowicz/20190708_Ens_Paper/other_paper/PDB_ids.txt #list of PDB names
+base_dir='/wynton/home/fraserlab/swankowicz/20190708_Ens_Paper/amber_refinement/' #location of folders with PDB files
+export OMP_NUM_THREADS=1
 
 #________________________________________________Activate Env________________________________________________#
+echo 'nslots'
 echo $NSLOTS
+echo 'sge':
 echo $SGE_TASK_ID
-source /wynton/home/fraserlab/swankowicz/phenix-1.15.2-3472/phenix_env.sh
-export PATH = "/wynton/home/fraserlab/swankowicz/anaconda3/bin:$PATH"
+#source /wynton/home/fraserlab/swankowicz/phenix-1.15.2-3472/phenix_env.sh
+source /wynton/home/fraserlab/swankowicz/phenix-1.8.2-1309/phenix_env.sh
+#source /wynton/home/fraserlab/swankowicz/phenix-installer-dev-3580-intel-linux-2.6-x86_64-centos6/phenix-dev-3580/phenix_env.sh
+#source /wynton/home/fraserlab/swankowicz/phenix-installer-dev-3594-intel-linux-2.6-x86_64-centos6/phenix-dev-3594/phenix_env.sh
+export PATH="/wynton/home/fraserlab/swankowicz/anaconda3/bin:$PATH"
 source activate phenix_ens
 which python
 
@@ -26,5 +32,5 @@ PDB = $(cat $input_file | head -n $SGE_TASK_ID | tail -n 1)
 echo $PDB
 cd $base_dir
 cd $PDB
-/wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/phenix_prep.sh $PDB $NSLOTS
+/wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/phenix_prep.sh $PDB $NSLOTS $base_dir
 
