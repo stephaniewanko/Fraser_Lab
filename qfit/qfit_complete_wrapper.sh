722a5462a8990d8eb4cfb8629c596a30c7c0e786
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2019-11-07 11:31:52 -0800

    Create qfit_complete_wrapper.sh

---
+++ qfit/qfit_complete_wrapper.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Stephanie Wankowicz
+#07/31/2019
+
+#this must be done before you submit to SGE since SGE cannot connect to the internet!
+source /home/wankowicz/phenix-installer-1.16-3546-intel-linux-2.6-x86_64-centos6/phenix-1.16-3546/phenix_env.sh
+#________________________________________________INPUTS________________________________________________#
+base_folder='/mnt/home1/wankowicz/190709_qfit/'
+
+pdb_filelist=/mnt/home1/wankowicz/190709_qfit/PDBs_holo.txt #/mnt/home1/wankowicz/scripts/PDBs_holo.txt
+#apo_190903_PDBs.txt #PDBs_holo.txt #/mnt/home1/wankowicz/scripts/apo_190903_PDBs.txt  #=/mnt/home1/wankowicz/190709_qfit/PDBs_holo.txt #list of pdb files
+while read -r line; do
+  PDB=$line
+  echo $PDB
+  cd $base_folder
+  sh /home/wankowicz/scripts/qfit_complete.sh $PDB $base_folder
+done < $pdb_filelist
