5a6284b67d20d7d1e482fa4da80f0cafa1e174cc
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2020-02-12 14:30:39 -0800

    Update ens_refine_gridsearch.sh
    updated wynton specific lines

--- phenix_pipeline/ens_refine_gridsearch.sh
+++ phenix_pipeline/ens_refine_gridsearch.sh
@@ -1,13 +1,12 @@
 !/bin/bash
+#grid search for ensemble_refinement
+#Stephanie Wankowicz 
+#10/26/2019
 
 #$ -l h_vmem=80G
 #$ -l mem_free=64G
 #$ -l h_rt=100:00:00
 #$ -t 1-45
-
-#grid search for ensemble_refinement
-#Stephanie Wankowicz 
-#10/26/2019
 
 source /wynton/home/fraserlab/swankowicz/phenix-installer-dev-3594-intel-linux-2.6-x86_64-centos6/phenix-dev-3594/phenix_env.sh
 export PATH="/wynton/home/fraserlab/swankowicz/anaconda3/bin:$PATH"
@@ -18,43 +17,40 @@ which python
 
 PDB=$1
 base_dir=$2 
-cd $working_dir
-echo $PDB
-cd $PDB
+cd ${base_dir}
+echo ${PDB}
+cd ${PDB}
 
-if [[ -z "$TMPDIR" ]]; then
-  if [[ -d /scratch ]]; then TMPDIR=/scratch/$USER; else TMPDIR=/tmp/$USER; fi
-  mkdir -p "$TMPDIR"
-  export TMPDIR
-fi
+#WYNTON SPECIFIC
+#if [[ -z "${TMPDIR}" ]]; then
+#  if [[ -d /scratch ]]; then TMPDIR=/scratch/$USER; else TMPDIR=/tmp/$USER; fi
+#  mkdir -p "$TMPDIR"
+#  export TMPDIR
+#fi
 
-cd "$TMPDIR"
+#cd "$TMPDIR"
 
-cp -R ${base_dir}/${PDB}/ $TMPDIR
-cd $PDB
+#cp -R ${base_dir}/${PDB}/ $TMPDIR
+#cd $PDB
 
-echo $PWD
+#echo $PWD
 
-options_file=/wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/phenix_ens_grid_search_options.txt
+options_file=phenix_ens_grid_search_options.txt
 _pTLS=$(cat $options_file | awk '{ print $1 }' |head -n $SGE_TASK_ID | tail -n 1)
 _weights=$(cat $options_file | awk '{ print $2 }'|head -n $SGE_TASK_ID | tail -n 1)
 _tx=$(cat $options_file | awk '{ print $3 }'|head -n $SGE_TASK_ID | tail -n 1)
 
-echo '_pTLS'
+echo 'pTLS:'
 echo $_pTLS
-echo 'weights'
+echo 'weights:'
 echo $_weights
-echo 'tx'
+echo 'tx:'
 echo $_tx
-
-echo $PWD
-
 
 lig_list=$(cat ${base_dir}/${PDB}/ligand_list.txt | rev | cut -c 5- | rev)
 
 
-
-output_file_name="${PDB}"_"${_pTLS}"_"${_weights}"_${_tx}
+output_file_name="${PDB}"_"${_pTLS}"_"${_weights}"_"${_tx}"
 echo $output_file_name
 
 if [ -d "/$output_file_name" ]; then
@@ -62,6 +58,7 @@ if [ -d "/$output_file_name" ]; then
 else
   mkdir $output_file_name
 fi
+
 cd $output_file_name
 
 if [ -z "$lig_list" ]; then
@@ -83,4 +80,4 @@ else
   fi
 fi
 
-cp -R ${TMPDIR}/$PDB/ $base_dir/
+#cp -R ${TMPDIR}/$PDB/ $base_dir/
