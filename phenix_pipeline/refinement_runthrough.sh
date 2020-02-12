c66332e9026ca56a4b1d422d450600a6aa40188e
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2020-02-12 14:37:21 -0800

    Update refinement_runthrough.sh
    updating to remove wynton specific items

--- phenix_pipeline/refinement_runthrough.sh
+++ phenix_pipeline/refinement_runthrough.sh
@@ -4,7 +4,7 @@
 #updated: 11-01-2019
 
 #source Phenix
-source /wynton/home/fraserlab/swankowicz/phenix-installer-dev-3594-intel-linux-2.6-x86_64-centos6/phenix-dev-3594/phenix_env.sh
+#source /wynton/home/fraserlab/swankowicz/phenix-installer-dev-3594-intel-linux-2.6-x86_64-centos6/phenix-dev-3594/phenix_env.sh
 export PHENIX_OVERWRITE_ALL=true
 
 PDB=$1
@@ -13,17 +13,11 @@ NSLOTS=$2
 echo $PDB
 echo $PWD  
 
-#if you want to run amber
-#phenix.AmberPrep ${PDB}.pdb
-
-
 #get ligand name and list of ligands for harmonic restraints
-~/anaconda3/envs/phenix_ens/bin/python /wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/PDB_ligand_parser.py $PDB /wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/ligands_to_remove.csv
+python PDB_ligand_parser.py $PDB igands_to_remove.csv
 lig_name=$(cat "ligand_name.txt")
 echo $lig_name
 
-echo '________________________________________________________Starting Phenix elbow________________________________________________________'
-#phenix.elbow $PDB.pdb --residue $lig_name --final_geometry
 
 echo '________________________________________________________Starting Phenix cif as mtz________________________________________________________'
 phenix.cif_as_mtz $PDB-sf.cif --extend_flags --merge
@@ -37,22 +31,22 @@ if grep -F _refln.F_meas_au $PDB-sf.cif; then
 else
         echo 'IOBS'
 fi
-rm ${PDB}.updated_refine_*
+
 if [[ -e "${PDB}.ligands.cif" ]]; then
 echo '________________________________________________________Running refinement with ligand.________________________________________________________'
     if grep -F _refln.F_meas_au $PDB-sf.cif; then
-        phenix.refine $PDB.updated.pdb $PDB-sf.mtz ${PDB}.ligands.cif refinement.input.xray_data.r_free_flags.label=R-free-flags /wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/finalize.params nproc=$NSLOTS refinement.input.xray_data.labels="FOBS,SIGFOBS"
+        phenix.refine $PDB.updated.pdb $PDB-sf.mtz ${PDB}.ligands.cif finalize.params nproc=$NSLOTS #refinement.input.xray_data.r_free_flags.label=R-free-flags refinement.input.xray_data.labels="FOBS,SIGFOBS"
     else
         echo 'IOBS'   
-         phenix.refine $PDB.updated.pdb $PDB-sf.mtz 4phenix_${PDB}.ligands.cif refinement.input.xray_data.r_free_flags.label=R-free-flags /wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/finalize.params nproc=$NSLOTS refinement.input.xray_data.labels="IOBS,SIGIOBS"
+         phenix.refine $PDB.updated.pdb $PDB-sf.mtz ${PDB}.ligands.cif finalize.params nproc=$NSLOTS #refinement.input.xray_data.r_free_flags.label=R-free-flags refinement.input.xray_data.labels="IOBS,SIGIOBS"
     fi
     
 else
     echo '________________________________________________________Running refinement without ligand.________________________________________________________'
     if grep -F _refln.F_meas_au $PDB-sf.cif; then
-        phenix.refine $PDB.updated.pdb $PDB-sf.mtz /wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/finalize.params nproc=$NSLOTS refinement.input.xray_data.labels="FOBS,SIGFOBS" refinement.input.xray_data.r_free_flags.label=R-free-flags 
+        phenix.refine $PDB.updated.pdb $PDB-sf.mtz finalize.params nproc=$NSLOTS #refinement.input.xray_data.labels="FOBS,SIGFOBS" refinement.input.xray_data.r_free_flags.label=R-free-flags 
     else
-        phenix.refine $PDB.updated.pdb $PDB-sf.mtz /wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/finalize.params nproc=$NSLOTS refinement.input.xray_data.r_free_flags.label=R-free-flags refinement.input.xray_data.labels="IOBS,SIGIOBS"
+        phenix.refine $PDB.updated.pdb $PDB-sf.mtz finalize.params nproc=$NSLOTS #refinement.input.xray_data.r_free_flags.label=R-free-flags refinement.input.xray_data.labels="IOBS,SIGIOBS"
    fi
  fi
 
@@ -60,15 +54,13 @@ echo '________________________________________________________Starting Model ver
 phenix.model_vs_data ${PDB}.updated_refine_001.pdb $PDB-sf.cif > ${PDB}_model_v_data.txt
 
 echo '________________________________________________________Starting extract python script________________________________________________________'
-  ~/anaconda3/envs/phenix_ens/bin/python /wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/Parse_refine_log.py -log_file ${PDB}.updated_refine_001.log -PDB $PDB
-  rm lig_RMSZ_updated.txt
-  rm lig_RMSZ_pre_refine.txt
+  python Parse_refine_log.py -log_file ${PDB}.updated_refine_001.log -PDB $PDB
   
   echo '________________________________________________________Validating the Ligand from Original PDB________________________________________________________'
   if [[ -e "${PDB}.ligands.cif" ]]; then  #only running this if we have a ligand    
     
     echo $lig_name
-    #mmtbx.validate_ligands ${PDB}.pdb ${PDB}-sf.mtz ligand_code=$lig_name #"${PDB}_ligand" #prints out ADPs and occs + additional information
+    mmtbx.validate_ligands ${PDB}.pdb ${PDB}-sf.mtz ligand_code=$lig_name #"${PDB}_ligand" #prints out ADPs and occs + additional information
     
     echo '________________________________________________________Phenix PDB Interpretation_______________________________________________________'
     phenix.pdb_interpretation ${PDB}.updated.pdb ${PDB}.ligands.cif write_geo=True
@@ -80,7 +72,7 @@ echo '________________________________________________________Starting extract p
 
 echo '________________________________________________________Validating the Ligand after Initial Refinement________________________________________________________'
   if [[ -e "${PDB}.ligands.cif" ]]; then  #only running this if we have a ligand
-    #mmtbx.validate_ligands ${PDB}.updated_refine_001.pdb ${PDB}-sf.mtz ligand_code=$lig_name #"${PDB}_ligand" #prints out ADPs and occs + additional information
+    mmtbx.validate_ligands ${PDB}.updated_refine_001.pdb ${PDB}-sf.mtz ligand_code=$lig_name #"${PDB}_ligand" #prints out ADPs and occs + additional information
     
     echo '________________________________________________________Phenix PDB Interpretation_______________________________________________________'
     phenix.pdb_interpretation ${PDB}.updated_refine_001.pdb ${PDB}.ligands.cif write_geo=True
@@ -92,9 +84,9 @@ echo '________________________________________________________Validating the Lig
   fi
 
   echo '________________________________________________________Validating Ligand Output_______________________________________________________'
-  ~/anaconda3/envs/phenix_ens/bin/python /wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/lig_geo_parser.py -pre_refine=lig_RMSZ_pre_refine.txt -post_refine=lig_RMSZ_updated.txt -PDB=$PDB
+  python lig_geo_parser.py -pre_refine=lig_RMSZ_pre_refine.txt -post_refine=lig_RMSZ_updated.txt -PDB=$PDB
 
 
   echo '________________________________________________________Begin Ensemble Refinement_______________________________________________________'
-  qsub /wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/grid_search_ens_refine.sh $PDB $3
+  qsub grid_search_ens_refine.sh $PDB $3
 
