3f7657862cf6af8a497d643837c0324dd359e791
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2020-03-24 18:52:11 -0400

    finished all lung subset

--- QCRG_COVID19/gtex_subset.py
+++ QCRG_COVID19/gtex_subset.py
@@ -2,11 +2,11 @@ import pandas as pd
 import numpy as np
 
 #import GTEX
-GTEX = pd.read_csv('/wynton/group/fraser/swankowicz/COVID19/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_median_tpm.gct', comment='#', sep='\t', header=1, low_memory=False)
+GTEX = pd.read_csv('GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_median_tpm.gct', comment='#', sep='\t', header=1, low_memory=False)
 
 #import gene lists
-drugs = pd.read_csv('/wynton/group/fraser/swankowicz/COVID19/drug_targets.txt', header=None)
-genes = pd.read_csv('/wynton/group/fraser/swankowicz/COVID19/final_targets.txt', header=None)
+drugs = pd.read_csv('drug_targets.txt', header=None)
+genes = pd.read_csv('final_targets.txt', header=None)
 
 #subset gtex for interacting proteins
 genes = genes.rename(columns={0: "Genes"})
@@ -22,5 +22,5 @@ gtex_subset = GTEX[GTEX['Description'].isin(drugs_np)]
 gtex_subset.to_csv('gtex_drugs_target.csv', sep=',', index=False)
 
 #subset gtex for all lung expression
-gtex_subset = GTEX[['Description', 'Lung']]
-
+gtex_all_lung = GTEX[['Description', 'Lung']]
+gtex_all_liung.to_csv('gtex_all_lung.csv', sep=',', index=False)
